9da45aad16eae96d5a48aeacd4522b5e86caad54
pratcaph <pratcaph@pc-dg-037-13.iut2.upmf-grenoble.fr>
2018-06-07 18:11:26 +0200

    Aventurier.estRole + nettoyage carteutilisable Hugo

--- src/Enumeration/CarteUtilisable.java
+++ src/Enumeration/CarteUtilisable.java
@@ -18,30 +18,10 @@ public enum CarteUtilisable {
     
     //methodes next et getNext n'ont pas été utiles pour le moment, à supprimer à la fin.
     
-    public CarteUtilisable next(CarteUtilisable c){
-
-        //on recup l'int de c;
-        //on marque enum de c +1
-        CarteUtilisable[] carte = CarteUtilisable.values();
-        int i = 0;
-        //compareTO renvoie :
-            //-1 si objet inf
-            // 0 si objet est le même
-            // 1 si l'objet est supérieur
-        
-        while (c.compareTo(carte[i])>=0||i<carte.length){
-            i++;
-            //si la carte est supérieur OU la même alors on incrémente
-            
-        }
-        c = carte[(i == carte.length ? 0:i)]; //soit 0 (hélico) soit i si la carte est pas à la fin
-        return c;
-    }
-    
     public CarteUtilisable getNext() {
      return this.ordinal() < CarteUtilisable.values().length - 1
-         ? CarteUtilisable.values()[this.ordinal() + 1]
-         : null;
+            ? CarteUtilisable.values()[this.ordinal() + 1]
+            : null;
    }
     
     
