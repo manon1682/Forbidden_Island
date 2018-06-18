c314fd9391f0da7baefc15dd7f410b30afa88027
cdlk <cdlk@Bernadette-V3>
2018-06-18 18:00:47 +0200

    VuePlateau whalla Hugo

--- src/view/VuePlateau.java
+++ src/view/VuePlateau.java
@@ -5,6 +5,13 @@
  */
 package view;
 
+import forbidden_island.Grille;
+import forbidden_island.Tuile;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 
 /**
@@ -13,10 +20,61 @@ import javax.swing.JPanel;
  */
 public class VuePlateau extends JPanel{
     
-    public VuePlateau(){
+    
+    
+    private VueTuile[][] tuiles;
+    
+    public VuePlateau(Grille grille){
+        tuiles = new VueTuile[6][6];
+        initTuiles(grille);
+        this.setLayout(new GridLayout(6,6));
+        Tuile tuile;
+        int l = 0;
+        int c = 0;
+        for(int i = 0; i<36 ; i++){
+            l = (c == 5? l+1 : l);
+            c = (c == 5? 0 : c+1);
+            if(grille.getTuiles()[l][c] != null){
+                tuile = grille.getTuiles()[l][c];
+                this.add(new VueTuile(tuile.getNom(),tuile.getEtat(), new Dimension(50, 50)));
+            } else {
+                JPanel pan = new JPanel();
+                pan.setBackground(Color.blue);
+                this.add(pan);
+            }
+        }
+        
+    }
+    
+    @Override
+    public void paintComponents(Graphics g){
+        
         
         
     }
     
+    public void majTuiles(Grille grille){
+        Tuile[][] ts = grille.getTuiles();
+        for (int l = 0; l < 6; l++) {
+            for (int c = 0; c < 6; c++) {
+                if(grille.getTuiles()[l][c] != null){
+                    tuiles[l][c].setNomTuile(ts[l][c].getNom());
+                    tuiles[l][c].setEtat(ts[l][c].getEtat());
+                }
+            }
+        }    
+    }
     
+    public void initTuiles(Grille grille){
+        Tuile[][] ts = grille.getTuiles();
+        for (int l = 0; l < 6; l++) {
+            for (int c = 0; c < 6; c++) {
+                if(grille.getTuiles()[l][c] != null){
+                    tuiles[l][c] = new VueTuile(ts[l][c].getNom(),ts[l][c].getEtat(), new Dimension(50, 50));
+                } else {
+                    tuiles[l][c] = null;
+                }
+            }
+        }
+    }
 }
