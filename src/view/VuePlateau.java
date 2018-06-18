93120ea7c099a4aa6b857d2b7455d4a33f5f2d54
cdlk <cdlk@Bernadette-V3>
2018-06-18 18:22:54 +0200

    VuePlateau finis Hugo

--- src/view/VuePlateau.java
+++ src/view/VuePlateau.java
@@ -5,6 +5,7 @@
  */
 package view;
 
+import Enumeration.EtatTuile;
 import forbidden_island.Grille;
 import forbidden_island.Tuile;
 import java.awt.Color;
@@ -18,61 +19,60 @@ import javax.swing.JPanel;
  *
  * @author cdlk
  */
-public class VuePlateau extends JPanel{
-    
-    
-    
-    private VueTuile[][] tuiles;
-    
-    public VuePlateau(Grille grille){
+public class VuePlateau extends JPanel {
+
+    private JPanel[][] tuiles;
+
+    public VuePlateau(Grille grille) {
         tuiles = new VueTuile[6][6];
         initTuiles(grille);
-        this.setLayout(new GridLayout(6,6));
-        Tuile tuile;
-        int l = 0;
-        int c = 0;
-        for(int i = 0; i<36 ; i++){
-            l = (c == 5? l+1 : l);
-            c = (c == 5? 0 : c+1);
-            if(grille.getTuiles()[l][c] != null){
-                tuile = grille.getTuiles()[l][c];
-                this.add(new VueTuile(tuile.getNom(),tuile.getEtat(), new Dimension(50, 50)));
-            } else {
-                JPanel pan = new JPanel();
-                pan.setBackground(Color.blue);
-                this.add(pan);
+        this.setLayout(new GridLayout(6, 6));
+        for (int l = 0; l < 6; l++) {
+            for (int c = 0; c < 6; c++) {
+            this.add(tuiles[l][c]);
             }
         }
-        
+
     }
-    
+
     @Override
-    public void paintComponents(Graphics g){
-        
-        
-        
-    }
-    
-    public void majTuiles(Grille grille){
-        Tuile[][] ts = grille.getTuiles();
+    public void paintComponents(Graphics g) {
         for (int l = 0; l < 6; l++) {
             for (int c = 0; c < 6; c++) {
-                if(grille.getTuiles()[l][c] != null){
-                    tuiles[l][c].setNomTuile(ts[l][c].getNom());
-                    tuiles[l][c].setEtat(ts[l][c].getEtat());
+                if (tuiles[l][c] != null) {
+                    tuiles[l][c].repaint();
                 }
             }
-        }    
+        }
+        this.repaint();
     }
-    
-    public void initTuiles(Grille grille){
+
+    public void majTuiles(Grille grille) {
         Tuile[][] ts = grille.getTuiles();
         for (int l = 0; l < 6; l++) {
             for (int c = 0; c < 6; c++) {
-                if(grille.getTuiles()[l][c] != null){
-                    tuiles[l][c] = new VueTuile(ts[l][c].getNom(),ts[l][c].getEtat(), new Dimension(50, 50));
+                if (grille.getTuiles()[l][c] != null && grille.getTuiles()[l][c].getEtat() != EtatTuile.coulée) {
+                    ((VueTuile) tuiles[l][c]).setNomTuile(ts[l][c].getNom());
+                    ((VueTuile) tuiles[l][c]).setEtat(ts[l][c].getEtat());
                 } else {
-                    tuiles[l][c] = null;
+                    JPanel pan = new JPanel();
+                    pan.setBackground(Color.blue);
+                    tuiles[l][c] = pan;
+                }
+            }
+        }
+    }
+
+    public void initTuiles(Grille grille) {
+        Tuile[][] ts = grille.getTuiles();
+        for (int l = 0; l < 6; l++) {
+            for (int c = 0; c < 6; c++) {
+                if (grille.getTuiles()[l][c] != null) {
+                    tuiles[l][c] = new VueTuile(ts[l][c].getNom(), ts[l][c].getEtat(), new Dimension(50, 50));
+                } else {
+                    JPanel pan = new JPanel();
+                    pan.setBackground(Color.blue);
+                    tuiles[l][c] = pan;
                 }
             }
         }
