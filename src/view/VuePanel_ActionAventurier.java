72c68395dfc58050bb4029b884fca1e320b95aa4
Blanquet Antoine <antoine.blanquet365@gmail.com>
2018-06-22 16:54:01 +0200

    Merge origin/master
    Conflicts:
    	src/view/VuePanel_ActionAventurier.java

--- src/view/VuePanel_ActionAventurier.java
+++ src/view/VuePanel_ActionAventurier.java
@@ -7,11 +7,18 @@ package view;
 
 import Enumeration.TypesMessages;
 import forbidden_island.Message;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -25,6 +32,7 @@ public class VuePanel_ActionAventurier extends JPanel {
 
     private JButton btnDeplacer, btnAssecher, btnActionSpeciale, btnTerminerTour;
     private JLabel nbActionText, nbActionInt;
+    private Image imageFond;
 
     private IHMJeu ihm;
 
@@ -35,13 +43,13 @@ public class VuePanel_ActionAventurier extends JPanel {
 
         //btnDeplacer
         btnDeplacer = new JButton();
-        ImageIcon logoDeplacer = new ImageIcon("images/buttons/buttonDeplacer2.png");
-        JLabel labelDep = new JLabel();    
-        Image imageDeplacer = logoDeplacer.getImage(); 
+        ImageIcon logoDeplacer = new ImageIcon("images/buttons/buttonDeplacer3.png");
+        JLabel labelDep = new JLabel();
+        Image imageDeplacer = logoDeplacer.getImage();
         Image newimgDeplacer = imageDeplacer.getScaledInstance(150, 75, java.awt.Image.SCALE_SMOOTH);
-        logoDeplacer = new ImageIcon(newimgDeplacer); 
-        labelDep.setIcon(logoDeplacer);   
-        
+        logoDeplacer = new ImageIcon(newimgDeplacer);
+        labelDep.setIcon(logoDeplacer);
+
         btnDeplacer.setIcon(logoDeplacer);
         btnDeplacer.setBackground(Color.red);
         btnDeplacer.setBorder(null);
@@ -49,13 +57,13 @@ public class VuePanel_ActionAventurier extends JPanel {
 
         //btnAssecher
         btnAssecher = new JButton();
-        ImageIcon logoAssecher = new ImageIcon("images/buttons/buttonAssecher2.png");
-        JLabel labelAss = new JLabel();    
-        Image imageAssecher = logoAssecher.getImage(); 
+        ImageIcon logoAssecher = new ImageIcon("images/buttons/buttonAssecher3.png");
+        JLabel labelAss = new JLabel();
+        Image imageAssecher = logoAssecher.getImage();
         Image newimgAssecher = imageAssecher.getScaledInstance(150, 75, java.awt.Image.SCALE_SMOOTH);
-        logoAssecher = new ImageIcon(newimgAssecher); 
-        labelAss.setIcon(logoAssecher);   
-        
+        logoAssecher = new ImageIcon(newimgAssecher);
+        labelAss.setIcon(logoAssecher);
+
         btnAssecher.setIcon(logoAssecher);
         btnAssecher.setBackground(Color.red);
         btnAssecher.setBorder(null);
@@ -63,63 +71,67 @@ public class VuePanel_ActionAventurier extends JPanel {
 
         //btn Action Spé
         btnActionSpeciale = new JButton();
-        ImageIcon logoActionSpeciale = new ImageIcon("images/buttons/buttonActionSpeciale2.png");
-        JLabel labelAP = new JLabel();    
-        Image imageActionSpeciale = logoActionSpeciale.getImage(); 
+        ImageIcon logoActionSpeciale = new ImageIcon("images/buttons/buttonActionSpeciale3.png");
+        JLabel labelAP = new JLabel();
+        Image imageActionSpeciale = logoActionSpeciale.getImage();
         Image newimgActionSpeciale = imageActionSpeciale.getScaledInstance(150, 75, java.awt.Image.SCALE_SMOOTH);
-        logoActionSpeciale = new ImageIcon(newimgActionSpeciale); 
-        labelAP.setIcon(logoActionSpeciale);   
-        
+        logoActionSpeciale = new ImageIcon(newimgActionSpeciale);
+        labelAP.setIcon(logoActionSpeciale);
+
         btnActionSpeciale.setIcon(logoActionSpeciale);
         btnActionSpeciale.setBackground(Color.red);
         btnActionSpeciale.setBorder(null);
-        btnActionSpeciale.setOpaque(false);        
-        
+        btnActionSpeciale.setOpaque(false);
+
         btnTerminerTour = new JButton();
-        ImageIcon logoTerminerTour = new ImageIcon("images/buttons/buttonTerminerTour2.png");
-        JLabel labelTT = new JLabel();    
-        Image imageTerminerTour = logoTerminerTour.getImage(); 
+        ImageIcon logoTerminerTour = new ImageIcon("images/buttons/buttonTerminerTour3.png");
+        JLabel labelTT = new JLabel();
+        Image imageTerminerTour = logoTerminerTour.getImage();
         Image newimgTerminerTour = imageTerminerTour.getScaledInstance(150, 75, java.awt.Image.SCALE_SMOOTH);
-        logoTerminerTour = new ImageIcon(newimgTerminerTour); 
-        labelTT.setIcon(logoTerminerTour);   
-        
+        logoTerminerTour = new ImageIcon(newimgTerminerTour);
+        labelTT.setIcon(logoTerminerTour);
+
         btnTerminerTour.setIcon(logoTerminerTour);
         btnTerminerTour.setBackground(Color.red);
         btnTerminerTour.setBorder(null);
-        btnTerminerTour.setOpaque(false); 
-        
-        
-        nbActionText = new JLabel("Nombre action dispo : ");
-        nbActionInt = new JLabel(Integer.toString(nbAction));
+        btnTerminerTour.setOpaque(false);
+
+        nbActionText = new JLabel();
+        ImageIcon logoNbActionText = new ImageIcon("images/buttons/nbaction.png");
+        Image imageNbActionText = logoNbActionText.getImage();
+        Image newimgNbActionText = imageNbActionText.getScaledInstance(150, 75, java.awt.Image.SCALE_SMOOTH); // Redimension de l'icône 
+        logoNbActionText = new ImageIcon(newimgNbActionText);
+        nbActionText.setIcon(logoNbActionText);
+
+//        nbActionInt = new JLabel(Integer.toString(nbAction));
+        nbActionInt = new JLabel();
+        ImageIcon logoNbActionInt = new ImageIcon("images/buttons/nombre3.png");
+        Image imageNbActionInt = logoNbActionInt.getImage();
+        Image newimgNbActionInt = imageNbActionInt.getScaledInstance(150, 75, java.awt.Image.SCALE_SMOOTH); // Redimension de l'icône 
+        logoNbActionInt = new ImageIcon(newimgNbActionInt);
+        nbActionInt.setIcon(logoNbActionInt);
         //fin initialisation
-
-        //test pour couleur :
-        //this.setBackground(Color.CYAN);
-
-        JPanel grilleAction = new JPanel(new GridLayout(1, 1));
-        JPanel grillebtnAction = new JPanel(new GridLayout(2, 2));
-        JPanel grilleNbAction = new JPanel(new GridLayout(2, 1));
-
-
         
-        grillebtnAction.add(btnDeplacer);
-        grillebtnAction.add(btnAssecher);
-        grillebtnAction.add(btnActionSpeciale);
-        
-        grillebtnAction.add(btnTerminerTour);
-        
-        grilleNbAction.add(nbActionText);
+        this.setLayout(new GridLayout(2, 3));
 
-        grilleNbAction.add(nbActionInt);
-        
-        grilleAction.add(grilleNbAction);
-        grilleAction.add(grillebtnAction);
+        this.add(btnDeplacer);
+        this.add(btnAssecher);
+        this.add(nbActionText);
+        this.add(btnActionSpeciale);
+        this.add(btnTerminerTour);
+        this.add(nbActionInt);
+
+//        grillebtnAction.setBackground(new Color(32,32,32));
+//        grillebtnAction.setBorder(BorderFactory.createLineBorder(Color.yellow, 3));
+        try {
+            this.imageFond = ImageIO.read(new FileInputStream("images/background/fondAction.png"));
+
+        } catch (IOException ex) {
+            System.err.println("Erreur de lecture de fondAction.png");
+        }
 
 
-        // grilleAction.add(new JLabel("")); //inutile ici
-        grilleAction.setOpaque(false);
-        this.add(grilleAction);
-
+//Action Listener
         btnDeplacer.addActionListener(
                 new ActionListener() {
             @Override
@@ -198,6 +210,12 @@ public class VuePanel_ActionAventurier extends JPanel {
 
     public void misAJourNbAction(int nbAction) {
         nbActionInt.setText(Integer.toString(nbAction));
+    }
+    
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        g.drawImage(imageFond, 0, 0, this.getWidth(), this.getHeight(), null, this);
     }
 
 }
