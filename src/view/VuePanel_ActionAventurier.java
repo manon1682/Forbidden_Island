e869d663ac85771c414326554b639bf9eeb36906
blancoma <blancoma@pc-dg-037-14.iut2.upmf-grenoble.fr>
2018-06-22 14:54:42 +0200

    recupere le defaite de mathias

--- src/view/VuePanel_ActionAventurier.java
+++ src/view/VuePanel_ActionAventurier.java
@@ -5,12 +5,8 @@
  */
 package view;
 
-import Aventurier.Aventurier;
-import Aventurier.Ingénieur;
-import Aventurier.Pilote;
 import Enumeration.TypesMessages;
 import forbidden_island.Message;
-import forbidden_island.Observe;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Image;
@@ -36,49 +32,49 @@ public class VuePanel_ActionAventurier extends JPanel {
 
         //initialisation
         ihm = ihmJ;
-        
+
         //btnDeplacer
         btnDeplacer = new JButton();
         ImageIcon logoDeplacer = new ImageIcon("images/buttons/buttonDeplacer.png");
-        JLabel labelDep = new JLabel();    
-        Image imageDeplacer = logoDeplacer.getImage(); 
+        JLabel labelDep = new JLabel();
+        Image imageDeplacer = logoDeplacer.getImage();
         Image newimgDeplacer = imageDeplacer.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
-        logoDeplacer = new ImageIcon(newimgDeplacer); 
-        labelDep.setIcon(logoDeplacer);   
-        
+        logoDeplacer = new ImageIcon(newimgDeplacer);
+        labelDep.setIcon(logoDeplacer);
+
         btnDeplacer.setIcon(logoDeplacer);
         btnDeplacer.setBackground(Color.red);
         btnDeplacer.setBorder(null);
         btnDeplacer.setOpaque(false);
-               
+
         //btnAssecher
         btnAssecher = new JButton();
         ImageIcon logoAssecher = new ImageIcon("images/buttons/buttonAssecher.png");
-        JLabel labelAss = new JLabel();    
-        Image imageAssecher = logoAssecher.getImage(); 
+        JLabel labelAss = new JLabel();
+        Image imageAssecher = logoAssecher.getImage();
         Image newimgAssecher = imageAssecher.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
-        logoAssecher = new ImageIcon(newimgAssecher); 
-        labelAss.setIcon(logoAssecher);   
-        
+        logoAssecher = new ImageIcon(newimgAssecher);
+        labelAss.setIcon(logoAssecher);
+
         btnAssecher.setIcon(logoAssecher);
         btnAssecher.setBackground(Color.red);
         btnAssecher.setBorder(null);
         btnAssecher.setOpaque(false);
-        
+
         //btn Action Spé
         btnActionSpeciale = new JButton();
         ImageIcon logoActionSpeciale = new ImageIcon("images/buttons/buttonActionSpeciale.png");
-        JLabel labelAP = new JLabel();    
-        Image imageActionSpeciale = logoActionSpeciale.getImage(); 
+        JLabel labelAP = new JLabel();
+        Image imageActionSpeciale = logoActionSpeciale.getImage();
         Image newimgActionSpeciale = imageActionSpeciale.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
-        logoActionSpeciale = new ImageIcon(newimgActionSpeciale); 
-        labelAP.setIcon(logoActionSpeciale);   
-        
+        logoActionSpeciale = new ImageIcon(newimgActionSpeciale);
+        labelAP.setIcon(logoActionSpeciale);
+
         btnActionSpeciale.setIcon(logoActionSpeciale);
         btnActionSpeciale.setBackground(Color.red);
         btnActionSpeciale.setBorder(null);
-        btnActionSpeciale.setOpaque(false);        
-        
+        btnActionSpeciale.setOpaque(false);
+
         btnTerminerTour = new JButton("Terminer Tour");
         nbActionText = new JLabel("Nombre action dispo : ");
         nbActionInt = new JLabel(Integer.toString(nbAction));
@@ -88,7 +84,6 @@ public class VuePanel_ActionAventurier extends JPanel {
         this.setBackground(Color.CYAN);
 
         JPanel grilleAction = new JPanel(new GridLayout(2, 3));
-
 
         grilleAction.add(btnDeplacer);
 
@@ -103,8 +98,6 @@ public class VuePanel_ActionAventurier extends JPanel {
         grilleAction.add(btnTerminerTour);
 
         // grilleAction.add(new JLabel("")); //inutile ici
-
-        
         grilleAction.setOpaque(false);
         this.add(grilleAction);
 
@@ -115,7 +108,7 @@ public class VuePanel_ActionAventurier extends JPanel {
                 ihm.setSauvType(TypesMessages.DEPLACER);
                 Message m = new Message(TypesMessages.DEPLACER);
                 ihm.notifierObservateur(m);
-                
+
                 btnDeplacer.setEnabled(false);
             }
 
@@ -128,7 +121,7 @@ public class VuePanel_ActionAventurier extends JPanel {
                 ihm.setSauvType(TypesMessages.ASSECHER);
                 Message m = new Message(TypesMessages.ASSECHER);
                 ihm.notifierObservateur(m);
-                
+
                 btnAssecher.setEnabled(false);
             }
 
@@ -141,7 +134,7 @@ public class VuePanel_ActionAventurier extends JPanel {
                 Message m = new Message(TypesMessages.SPECIALE);
                 ihm.setSauvType(TypesMessages.SPECIALE);
                 ihm.notifierObservateur(m);
-                
+
                 btnActionSpeciale.setEnabled(false);
             }
 
@@ -150,7 +143,7 @@ public class VuePanel_ActionAventurier extends JPanel {
         btnTerminerTour.addActionListener(
                 new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {              
+            public void actionPerformed(ActionEvent e) {
                 Message m = new Message(TypesMessages.TERMINER_TOUR);
                 ihm.notifierObservateur(m);
             }
@@ -172,8 +165,8 @@ public class VuePanel_ActionAventurier extends JPanel {
     public JButton getBtnTerminerTour() {
         return btnTerminerTour;
     }
-    
-    public void finirTour(){
+
+    public void finirTour() {
         //Activation ou non du bouton "Déplacer"
         getBtnDeplacer().setEnabled(false);
 
@@ -183,9 +176,9 @@ public class VuePanel_ActionAventurier extends JPanel {
         //Activation ou non du bouton "Action spéciale"
         getBtnActionSpeciale().setEnabled(false);
     }
-    
-    public void misAJourNbAction(int nbAction){
+
+    public void misAJourNbAction(int nbAction) {
         nbActionInt.setText(Integer.toString(nbAction));
     }
-    
+
 }
--- src/view/VuePanel_Defaite.java
+++ src/view/VuePanel_Defaite.java
@@ -9,7 +9,6 @@ import Enumeration.Defaite;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,24 +19,25 @@ import javax.swing.SwingConstants;
  * @author cabezama
  */
 public class VuePanel_Defaite extends JPanel {
+
     private JLabel motif;
-    
+
     public VuePanel_Defaite(Defaite d) {
         setLayout(new BorderLayout());
         setBackground(new Color(0, 0, 0, 210));
-        
+
         // NORD
-        ImageIcon logoIle= new ImageIcon("images/logo/ile-interdite-logo.png");
-        JLabel ile = new JLabel("",SwingConstants.CENTER);
+        ImageIcon logoIle = new ImageIcon("images/logo/ile-interdite-logo.png");
+        JLabel ile = new JLabel("", SwingConstants.CENTER);
         ile.setIcon(logoIle);
-        
+
         this.add(ile, BorderLayout.NORTH);
-        
+
         //CENTRE
         ImageIcon logoDef = new ImageIcon("images/defaite/defaite_photoshop.png");
-        JLabel def = new JLabel("",SwingConstants.CENTER);
-        def.setIcon(logoDef);      
-        
+        JLabel def = new JLabel("", SwingConstants.CENTER);
+        def.setIcon(logoDef);
+
         this.add(def, BorderLayout.CENTER);
 
         //SUD : Traitement des différentes défaites
@@ -45,34 +45,34 @@ public class VuePanel_Defaite extends JPanel {
             switch (d) {
                 case HELIPORT_COULE: {
 
-                    motif = new JLabel("Votre Héliport a coulé ! Vous n'avez pas pu vous échapper !",SwingConstants.CENTER);
+                    motif = new JLabel("Votre Héliport a coulé ! Vous n'avez pas pu vous échapper !", SwingConstants.CENTER);
                     motif.setFont(new Font("Serif", Font.ITALIC, 35));
-                    motif.setForeground(new Color(225,221,136));
+                    motif.setForeground(new Color(225, 221, 136));
                     break;
                 }
                 case INONDATION_ELEVEE: {
-                    motif = new JLabel("Niveau d'inondation trop élevé ! Vous vous êtes noyés !",SwingConstants.CENTER);
+                    motif = new JLabel("Niveau d'inondation trop élevé ! Vous vous êtes noyés !", SwingConstants.CENTER);
                     motif.setFont(new Font("Serif", Font.ITALIC, 35));
-                    motif.setForeground(new Color(225,221,136));
+                    motif.setForeground(new Color(225, 221, 136));
                     break;
                 }
                 case JOUEUR_NOYE: {
-                    motif = new JLabel("Un aventurier s'est noyé !",SwingConstants.CENTER);
+                    motif = new JLabel("Un aventurier s'est noyé !", SwingConstants.CENTER);
                     motif.setFont(new Font("Serif", Font.ITALIC, 35));
-                    motif.setForeground(new Color(225,221,136));
+                    motif.setForeground(new Color(225, 221, 136));
                     break;
                 }
                 case TRESOR_COULE: {
-                    motif = new JLabel("Un trésor a sombré !",SwingConstants.CENTER);
+                    motif = new JLabel("Un trésor a sombré !", SwingConstants.CENTER);
                     motif.setFont(new Font("Serif", Font.ITALIC, 35));
-                    motif.setForeground(new Color(225,221,136));
+                    motif.setForeground(new Color(225, 221, 136));
                     break;
                 }
                 default:
                     break;
             }
         }
-        
+
         this.add(motif, BorderLayout.SOUTH);
 
         setVisible(true);
